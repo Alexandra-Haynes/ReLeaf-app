e0270a215834ff32d33bab4c83fed1503c38e00e
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-08-14 09:41:52 -1000

    initial setup

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import PlantCard from "./PlantCard";
+import { CircleButton, RectButton } from "./Button";
+import { DetailsDesc } from "./DetailsDesc";
+import { DetailsBid } from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  PlantCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
---
+++ constants/assets.js
@@ -0,0 +1,43 @@
+import badge from "../assets/icons/badge.png";
+import dollar from "../assets/icons/dollar.png";
+import heart from "../assets/icons/heart.png";
+import heart_filled from "../assets/icons/heart_filled.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import plant01 from "../assets/images/plant01.jpg";
+import plant02 from "../assets/images/plant02.jpg";
+import plant03 from "../assets/images/plant03.jpg";
+import plant04 from "../assets/images/plant04.jpg";
+import plant05 from "../assets/images/plant05.jpg";
+import plant06 from "../assets/images/plant06.jpg";
+import plant07 from "../assets/images/plant07.jpg";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+
+export default {
+  badge,
+  dollar,
+  heart,
+  heart_filled,
+  left,
+  search,
+
+  logo,
+  plant01,
+  plant02,
+  plant03,
+  plant04,
+  plant05,
+  plant06,
+  plant07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
